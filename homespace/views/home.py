abacdad3f0a9a702357f5b033dd6d49d5cad14b0
Elisp12 <eliseuspdam.1999@gmail.com>
2024-08-18 21:15:59 -0300

    Views: listando produtos

--- homespace/views/home.py
+++ homespace/views/home.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from homespace.models import Categoria, Produto
+
+@login_required
 def home(request):
 
+    lista_produtos = Produto.objects.all()
+
     context = {
-        
+        'lista_produtos': lista_produtos
     }
     return render(request, 'index.html', context = context)
