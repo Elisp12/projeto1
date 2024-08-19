a81ff0b818351e27e46a0f9284ae3801176e8d29
Elisp12 <eliseuspdam.1999@gmail.com>
2024-08-18 21:41:54 -0300

    Criando arquivos státicos e mídia

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / 'static')
+]
+
+# MEdia
+
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
