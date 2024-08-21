ce9578e9c581565a903f4dc5ed057190062cb012
Elisp12 <eliseuspdam.1999@gmail.com>
2024-08-21 20:16:00 -0300

    filtro boleano

--- homespace/urls.py
+++ homespace/urls.py
@@ -8,5 +8,5 @@ from homespace.views.produto_ativo import *
 urlpatterns = [
     path('', lista_produto, name='lista_produto'),
     path('lista/produto/categoria/<int:index>/', lista_produto_categoria, name='lista_produto_categoria'),
-    path('lista/produto/ativo/<int:index>/', produto_ativo, name='lista_produto_ativo'),
+    path('lista/produto/ativo/', produto_ativo, name='lista_produto_ativo'),
 ]
--- homespace/views/produto_ativo.py
+++ homespace/views/produto_ativo.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 
 from homespace.models import Produto
 
-def produto_ativo(request, index):
-    ativo = Produto.objects.filter(ativo = index).all()
+# def produto_ativo(request, index):
+#     ativo = Produto.objects.filter(ativo = index).all()
+
+#     context = {
+#         'produto_ativo': ativo,
+#     }
+
+#     return render(request, 'produto/produto_ativo.html', context = context)
+
+def produto_ativo(request):
+
+    ativado =Produto.objects.filter(ativo = True).values('nome', 'descricao', 'quantidade', 'ativo', 'categoria', 'img', 'athor')
 
     context = {
-        'produto_ativo': ativo,
+        'produto_ativo': ativado,
     }
 
-    return render(request, 'produto/produto_ativo.html', context = context)
+    return render(request, 'produto/produto_ativo.html', context =context)
