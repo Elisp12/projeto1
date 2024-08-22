c9c39cc27fcf7321081f9e6144b83ec913d0ece6
Elisp12 <eliseuspdam.1999@gmail.com>
2024-08-21 22:39:07 -0300

    mostra nome de usuario athor do produto, por id

--- homespace/views/produto_usuario.py
+++ homespace/views/produto_usuario.py
@@ -5,9 +5,12 @@ from homespace.models import Produto
 def produto_usuario(request, id):
 
     lista = Produto.objects.filter(athor = id).values('nome', 'descricao', 'quantidade', 'ativo', 'categoria', 'img', 'athor')
+
+    usuario = User.objects.filter(id = id).values('username')
     
     context = {
         'produto_usuario': lista,
+        'usuario': usuario,
     }
 
     return render(request, 'usuario/produto_usuario.html', context = context)
